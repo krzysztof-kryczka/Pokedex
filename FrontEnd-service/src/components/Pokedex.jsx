1a3d2cc3b1f245788fe84fe886626d92fee15e1a
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-11-20 17:14:45 +0100

    created pagination component and petite styles for pagination

--- FrontEnd-service/src/components/Pokedex.jsx
+++ FrontEnd-service/src/components/Pokedex.jsx
@@ -1,39 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useFetchPokemons } from '../hooks/useFetchPokemons'
+import { Pagination } from './Pagination'
+import { Loader } from './Loader'
 
+const totalPokemons = 150
 const pokemonsPerPage = 15
+const totalPages = Math.ceil(totalPokemons / pokemonsPerPage)
 
 export const Pokedex = () => {
-   const { pokemons, loading, error } = useFetchPokemons(pokemonsPerPage)
-   console.log(pokemons.results)
+   const [currentPage, setCurrentPage] = useState(1)
+   const { pokemons, isLoading, error } = useFetchPokemons(pokemonsPerPage, currentPage)
+   console.log(pokemons)
 
-   if (loading) return <p className="text-center">Ładowanie...</p>
-   if (error) return <p class="text-center text-red-700 font-bold">Błąd podczas pobierania danych z API.</p>
+   const handlePageChange = pageNumber => {
+      setCurrentPage(pageNumber)
+   }
 
    return (
       <div className="p-4 max-auto mx-auto">
-         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 md:gap-12">
-            {pokemons.results.map(pokemon => {
-               console.log('pokemon.url.split', pokemon.url.split('/'))
-               return (
-                  <li key={pokemon.name} className="border rounded-lg overflow-hidden shadow-lg">
-                     <div className="p-4 hover:scale-105 transform transition-transform cursor-pointer">
-                        <h2 className="text-lg sm:text-xl md:text-4xl font-bold text-center uppercase">
-                           {pokemon.name}
-                        </h2>
-                        {/* SPRITE IMG: https://stackoverflow.com/a/71250760 */}
-                        <img
-                           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-                              pokemon.url.split('/')[6] // ["https:", "", "pokeapi.co", "api", "v2", "pokemon", "1", ""]
-                           }.svg`}
-                           alt={pokemon.name}
-                           className="mx-auto w-36 h-36 sm:w-48 sm:h-48 md:w-96 md:h-96 object-contain"
-                        />
-                     </div>
-                  </li>
-               )
-            })}
-         </ul>
+         {error && <p className="text-center text-red-700 font-bold">Błąd podczas pobierania danych z API.</p>}
+         {isLoading && <Loader />}
+         {!isLoading && !error && (
+            <>
+               <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 md:gap-12">
+                  {pokemons.results.map(pokemon => {
+                     console.log('pokemon.url.split', pokemon.url.split('/'))
+                     return (
+                        <li key={pokemon.name} className="border rounded-lg overflow-hidden shadow-lg">
+                           <div className="p-4 hover:scale-105 transform transition-transform cursor-pointer">
+                              <h2 className="text-lg sm:text-xl md:text-4xl font-bold text-center uppercase">
+                                 {pokemon.name}
+                              </h2>
+                              {/* SPRITE IMG: https://stackoverflow.com/a/71250760 */}
+                              <img
+                                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+                                    pokemon.url.split('/')[6] // ["https:", "", "pokeapi.co", "api", "v2", "pokemon", "1", ""]
+                                 }.svg`}
+                                 alt={pokemon.name}
+                                 className="mx-auto w-36 h-36 sm:w-48 sm:h-48 md:w-96 md:h-96 object-contain"
+                              />
+                           </div>
+                        </li>
+                     )
+                  })}
+               </ul>
+               <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+            </>
+         )}
       </div>
    )
 }
--- FrontEnd-service/src/hooks/useFetchPokemons.js
+++ FrontEnd-service/src/hooks/useFetchPokemons.js
@@ -1,26 +1,33 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export const useFetchPokemons = (pokemonsPerPage = 15) => {
+export const useFetchPokemons = (pokemonsPerPage = 15, currentPage = 1) => {
    const [pokemons, setPokemons] = useState([])
-   const [loading, setLoading] = useState(true)
+   const [isLoading, setIsLoading] = useState(true)
    const [error, setError] = useState(null)
 
    useEffect(() => {
       const fetchPokemons = async () => {
          try {
-            setLoading(true)
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${pokemonsPerPage}`)
+            setIsLoading(true)
+            /* currentPage = 1, pokemonsPerPage = 15, Offset: (1 - 1) * 15 = 0,  Get Pokémons 01 .. 15.
+               currentPage = 2, pokemonsPerPage = 15, Offset: (2 - 1) * 15 = 15, Get Pokémons 16 .. 30.
+               currentPage = 3, pokemonsPerPage = 15, Offset: (3 - 1) * 15 = 30, Get Pokémons 31 .. 45.
+               currentPage = 4, pokemonsPerPage = 15, Offset: (4 - 1) * 15 = 45, Get Pokémons 46 .. 60.
+            */
+            const offset = (currentPage - 1) * pokemonsPerPage
+            const response = await axios.get(
+               `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${pokemonsPerPage}`,
+            )
             setPokemons(response.data)
          } catch (err) {
             setError(err)
          } finally {
-            setLoading(false)
+            setIsLoading(false)
          }
       }
-
       fetchPokemons()
-   }, [pokemonsPerPage])
+   }, [pokemonsPerPage, currentPage])
 
-   return { pokemons, loading, error }
+   return { pokemons, isLoading, error }
 }
