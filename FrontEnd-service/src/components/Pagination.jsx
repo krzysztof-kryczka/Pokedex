ad8be43dadea359cf29b50016228a67a264a72d9
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-11-20 21:52:43 +0100

    refactor pagination component

--- FrontEnd-service/src/components/Pagination.jsx
+++ FrontEnd-service/src/components/Pagination.jsx
@@ -1,3 +1,16 @@
+const PaginationButton = ({ page, currentPage, onPageChange }) => (
+   <button
+      onClick={() => onPageChange(page)}
+      className={`w-8 h-8 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center justify-center text-sm sm:text-base md:text-3xl font-bold rounded-full ${
+         currentPage === page
+            ? 'text-white border-2 border-blue-500 bg-gray-300'
+            : 'bg-white text-blue-500 hover:bg-blue-500 hover:text-white hover:rounded-full'
+      }`}
+   >
+      {page}
+   </button>
+)
+
 export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
    const maxPages = 5
    const startPage = Math.max(1, Math.min(totalPages - maxPages + 1, currentPage - Math.floor(maxPages / 2)))
@@ -7,23 +20,9 @@ export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
    return (
       <div className="flex flex-wrap justify-center space-x-2 my-4">
          {currentPage > 1 && (
-            <button
-               onClick={() => onPageChange(currentPage - 1)}
-               className="w-8 h-8 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center justify-center text-sm sm:text-base md:text-3xl font-bold bg-white text-blue-500 hover:bg-blue-500 hover:text-white rounded-full"
-            >
-               &lt;
-            </button>
+            <PaginationButton page={currentPage - 1} currentPage={currentPage} onPageChange={onPageChange} />
          )}
-         <button
-            onClick={() => onPageChange(1)}
-            className={`w-8 h-8 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center justify-center text-sm sm:text-base md:text-3xl font-bold rounded-full ${
-               currentPage === 1
-                  ? 'text-white border-2 border-blue-500 bg-gray-300'
-                  : 'bg-white text-blue-500 hover:bg-blue-500 hover:text-white hover:rounded-full'
-            }`}
-         >
-            1
-         </button>
+         <PaginationButton page={1} currentPage={currentPage} onPageChange={onPageChange} />
          {startPage > 1 && startPage !== 2 && (
             <button
                className="w-8 h-8 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center justify-center text-sm sm:text-base md:text-3xl font-bold bg-white text-blue-500 hover:bg-blue-500 hover:text-white rounded-full"
@@ -35,17 +34,7 @@ export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
          {pages.map(
             page =>
                page !== 1 && (
-                  <button
-                     key={page}
-                     onClick={() => onPageChange(page)}
-                     className={`w-8 h-8 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center justify-center text-sm sm:text-base md:text-3xl font-bold rounded-full ${
-                        currentPage === page
-                           ? 'text-white border-2 border-blue-500 bg-gray-300'
-                           : 'bg-white text-blue-500 hover:bg-blue-500 hover:text-white hover:rounded-full'
-                     }`}
-                  >
-                     {page}
-                  </button>
+                  <PaginationButton key={page} page={page} currentPage={currentPage} onPageChange={onPageChange} />
                ),
          )}
          {endPage < totalPages && (
@@ -56,25 +45,11 @@ export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
                >
                   ...
                </button>
-               <button
-                  onClick={() => onPageChange(totalPages)}
-                  className={`w-8 h-8 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center justify-center text-sm sm:text-base md:text-3xl font-bold rounded-full ${
-                     currentPage === totalPages
-                        ? 'text-white border-2 border-blue-500 bg-gray-300'
-                        : 'bg-white text-blue-500 hover:bg-blue-500 hover:text-white hover:rounded-full'
-                  }`}
-               >
-                  {totalPages}
-               </button>
+               <PaginationButton page={totalPages} currentPage={currentPage} onPageChange={onPageChange} />
             </>
          )}
          {currentPage < totalPages && (
-            <button
-               onClick={() => onPageChange(currentPage + 1)}
-               className="w-8 h-8 sm:w-10 sm:h-10 md:w-14 md:h-14 flex items-center justify-center text-sm sm:text-base md:text-3xl font-bold bg-white text-blue-500 hover:bg-blue-500 hover:text-white rounded-full"
-            >
-               &gt;
-            </button>
+            <PaginationButton page={currentPage + 1} currentPage={currentPage} onPageChange={onPageChange} />
          )}
       </div>
    )
