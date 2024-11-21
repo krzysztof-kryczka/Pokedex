c9106cb5f34040c541ec70574c201f97d36c3c73
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-11-21 22:10:28 +0100

    added search filtered poke

--- FrontEnd-service/src/components/Pokedex.jsx
+++ FrontEnd-service/src/components/Pokedex.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useFetchPokemons } from '../hooks/useFetchPokemons'
 import { Pagination } from './Pagination'
 import { Loader } from './Loader'
@@ -10,6 +10,8 @@ const totalPages = Math.ceil(totalPokemons / pokemonsPerPage)
 export const Pokedex = () => {
    const { pokemons, isLoading, error, currentPage, setCurrentPage } = useFetchPokemons(pokemonsPerPage)
    console.log('pokemons', pokemons)
+   const [searchTerm, setSearchTerm] = useState('')
+   const filteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
    const handlePageChange = pageNumber => setCurrentPage(pageNumber)
 
@@ -17,10 +19,17 @@ export const Pokedex = () => {
       <div className="p-4 max-auto mx-auto">
          {error && <p className="text-center text-red-700 font-bold">Błąd podczas pobierania danych z API.</p>}
          {isLoading && <Loader />}
+         <input
+            type="text"
+            placeholder="Szukaj Pokémona"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+            className="p-2 border rounded w-full mb-4 md:p-3 md:mb-6 lg:p-4 lg:mb-8 bg-gradient-to-r from-blue-200 via-blue-50 to-blue-100 text-black"
+         />
          {!isLoading && !error && (
             <>
                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-8 md:gap-12">
-                  {pokemons.map(pokemon => {
+                  {filteredPokemons.map(pokemon => {
                      console.log('pokemon.url.split', pokemon.url.split('/'))
                      return (
                         <li
@@ -64,7 +73,9 @@ export const Pokedex = () => {
                      )
                   })}
                </ul>
-               <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+               {filteredPokemons.length >= pokemonsPerPage && (
+                  <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+               )}
             </>
          )}
       </div>
