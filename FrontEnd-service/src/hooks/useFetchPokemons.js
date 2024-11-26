bfe7d06eba7608983c536a9631d57fe5b4855baf
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-11-26 16:47:57 +0100

    fix: updated useFetchPokemons to properly handle local and external Pokémon sources. Refactor useFetchPokemons to correctly calculate offset when combining local and external Pokémon sources

--- FrontEnd-service/src/hooks/useFetchPokemons.js
+++ FrontEnd-service/src/hooks/useFetchPokemons.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export const useFetchPokemons = (pokemonsPerPage = 15) => {
+export const useFetchPokemons = (pokemonsPerPage = 15, includeLocalPokemons = false) => {
    const [pokemons, setPokemons] = useState([])
    const [isLoading, setIsLoading] = useState(true)
    const [error, setError] = useState(null)
@@ -11,36 +11,54 @@ export const useFetchPokemons = (pokemonsPerPage = 15) => {
       const fetchPokemons = async () => {
          try {
             setIsLoading(true)
-            /* currentPage = 1, pokemonsPerPage = 15, Offset: (1 - 1) * 15 = 0,  Get Pokémons 01 .. 15.
+            let finalPokemons = []
+            let totalLocalPokemons = 0
+
+            if (includeLocalPokemons) {
+               // Fetch Pokémon from local db.json
+               const dbResponse = await axios.get('http://localhost:3000/pokemons')
+               const dbPokemons = dbResponse.data
+               totalLocalPokemons = dbPokemons.length
+
+               // Include local Pokémons in the current Page
+               const localPokemonsPage = dbPokemons.slice(
+                  (currentPage - 1) * pokemonsPerPage,
+                  currentPage * pokemonsPerPage,
+               )
+               finalPokemons = [...localPokemonsPage]
+            }
+
+            const remainingPokemonsCount = pokemonsPerPage - finalPokemons.length
+            console.log('remainingPokemonsCount', remainingPokemonsCount)
+
+            if (remainingPokemonsCount > 0) {
+               /* currentPage = 1, pokemonsPerPage = 15, Offset: (1 - 1) * 15 = 0,  Get Pokémons 01 .. 15.
                currentPage = 2, pokemonsPerPage = 15, Offset: (2 - 1) * 15 = 15, Get Pokémons 16 .. 30.
                currentPage = 3, pokemonsPerPage = 15, Offset: (3 - 1) * 15 = 30, Get Pokémons 31 .. 45.
                currentPage = 4, pokemonsPerPage = 15, Offset: (4 - 1) * 15 = 45, Get Pokémons 46 .. 60.
             */
-            const offset = (currentPage - 1) * pokemonsPerPage
-            // Fetch Pokémon from external API
-            const apiResponse = await axios.get(
-               `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${pokemonsPerPage}`,
-            )
-            const detailedPokemons = await Promise.all(
-               apiResponse.data.results.map(async pokemon => {
-                  const pokemonResponse = await axios.get(pokemon.url)
-                  const pokemonDetails = pokemonResponse.data
-                  // console.log('Fetched Pokemon Details:', pokemonDetails)
-                  // const combinedData = { ...pokemon, ...pokemonDetails }
-                  // console.log('Combined Pokemon Details Data:', combinedData)
-                  return { ...pokemon, ...pokemonDetails }
-               }),
-            )
+               // Calculate offset, starting from 0 for API Pokémon
+               const offset = Math.max(0, (currentPage - 1) * pokemonsPerPage - totalLocalPokemons)
+               // const offset = (currentPage - 1) * remainingPokemonsCount
 
-            // Fetch Pokémon from local db.json
-            const dbResponse = await axios.get('http://localhost:3000/pokemons')
-            const dbPokemons = dbResponse.data
-
-            // Merge and deduplicate Pokémon lists
-            const mergedPokemons = [...detailedPokemons, ...dbPokemons]
-            console.log('mergedPokemons: ', mergedPokemons)
-            setPokemons(mergedPokemons)
-            //setPokemons(detailedPokemons)
+               // Fetch Pokémon from external API
+               const apiResponse = await axios.get(
+                  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${remainingPokemonsCount}`,
+               )
+               const detailedPokemons = await Promise.all(
+                  apiResponse.data.results.map(async pokemon => {
+                     const pokemonResponse = await axios.get(pokemon.url)
+                     const pokemonDetails = pokemonResponse.data
+                     // console.log('Fetched Pokemon Details:', pokemonDetails)
+                     // const combinedData = { ...pokemon, ...pokemonDetails }
+                     // console.log('Combined Pokemon Details Data:', combinedData)
+                     return { ...pokemon, ...pokemonDetails }
+                  }),
+               )
+               finalPokemons = [...finalPokemons, ...detailedPokemons]
+               console.log(finalPokemons)
+            }
+            setPokemons(finalPokemons)
          } catch (err) {
             setError(err)
          } finally {
@@ -48,7 +66,7 @@ export const useFetchPokemons = (pokemonsPerPage = 15) => {
          }
       }
       fetchPokemons()
-   }, [pokemonsPerPage, currentPage])
+   }, [pokemonsPerPage, currentPage, includeLocalPokemons])
 
    return { pokemons, isLoading, error, currentPage, setCurrentPage }
 }
